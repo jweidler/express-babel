484c9f5e2b78579bb42f519c7e81fe022ed9a169
Jonas Weidler <mail@jonas-weidler.de>
2015-04-20 13:34:10 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+/*jslint node: true */
+"use strict";
+
+var gulp = require('gulp'),
+    gls = require('gulp-live-server'),
+    babel = require('gulp-babel'),
+    gulpCopy = require('gulp-copy'),
+    path = require('path');
+
+var buildPath = 'build';
+var srcPath = 'src';
+var ecmaScriptFiles = [
+    './'+srcPath+'/*.js',
+    './'+srcPath+'/**/*.js'
+];
+var filesToCopy = [
+    './'+srcPath+'/**/*.jade',
+    './'+srcPath+'/public/*.*',
+    './'+srcPath+'/public/**/*.*'
+];
+
+gulp.task('buildEcmaScriptFiles', function() {
+    return gulp.src(ecmaScriptFiles)
+        .pipe(babel())
+        .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('copyAssets', function() {
+    return gulp.src(filesToCopy)
+        .pipe(gulpCopy(buildPath, {prefix: 1}));
+});
+
+gulp.task('serve', ['buildEcmaScriptFiles', 'copyAssets'], function() {
+    var server = gls.new('./bin/www');
+    server.start();
+    gulp.watch(filesToCopy, ['copyAssets']);
+    gulp.watch(ecmaScriptFiles, ['buildEcmaScriptFiles', server.start]);
+});
